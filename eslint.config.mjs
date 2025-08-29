7068c10e7b5a6f0a0eeaff8430e4bbd93cff637e
Arjun Puri <arjun_261094@hotmail.com>
2025-08-29 19:22:28 +0200

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import typescript from '@typescript-eslint/parser';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended', 'plugin:@typescript-eslint/recommended'),
+  {
+    languageOptions: {
+      parser: typescript,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      'prettier/prettier': 'warn',
+      'react/no-unescaped-entities': 'off',
+    },
+  },
+];
+
+export default eslintConfig;
---
+++ src/app/(homepage)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-16 p-8 pb-20 sm:p-20">
+      <h1 className="font-raleway text-4xl font-bold">Get Started!</h1>
+    </div>
+  );
+};
+
+export default Home;
